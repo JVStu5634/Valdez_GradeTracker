b077d8135b07c84caf8032367330239a1978a95b
JVStu5634 <valdezjoseph123@gmail.com>
2026-03-31 19:10:38 -0500

    Update grades.h

--- grades.h
+++ grades.h
@@ -1,1 +1,22 @@
+// Name: Joseph Valdez
+// Date: 3/31/2026
+// File: grades.h
+// Purpose: Define an enum called LetterGrade with values A, B, C, D, F. Declare a function called getLetterGrade and 
+// declare a function called getLetterGrade and  a function called gradeToChar that takes a LetterGrade and returns a char.
 
+#ifndef GRADES_H
+#define Grades_H
+
+enum class LetterGrade { 
+A,
+B,
+C,
+D,
+F
+};
+
+LetterGrade getLettergrade(double score);
+
+char gradeToChar(LetterGrade grade);
+
+#endif
