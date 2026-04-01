2fabc87c1c9e3eb13ab8f5df26f4ee697e924824
JVStu5634 <valdezjoseph123@gmail.com>
2026-03-31 23:31:09 -0500

    Create stats.h

---
+++ stats.h
@@ -0,0 +1,11 @@
+// Name: Joseph Valdez
+// Date: 3/31/2026
+// File: stats.h
+// Purpose: This file declares the statistical utility function.
+
+#ifndef STATS_H
+#define STATS_H
+
+double computeAverage(double total, int count); 
+
+#endif
